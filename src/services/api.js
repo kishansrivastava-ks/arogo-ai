5356a95c0384535d062857dd28ab6fdf70b73287
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2025-03-10 16:05:22 +0530

    auth issue check

--- src/services/api.js
+++ src/services/api.js
@@ -6,17 +6,17 @@ const API = axios.create({
   withCredentials: true,
 });
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default API;
